7bd4fbbfb11c26a844092b96b500b4ea4228cc43
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2023-01-08 00:51:02 +0200

    Add Ex10.3: the reviewed repositories list

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+})
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <View style={styles}>
+      <Text>Full name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Reviews: {item.reviewCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+    </View>
+  )
+}
+
+export default RepositoryItem
