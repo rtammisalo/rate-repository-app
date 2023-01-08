cbabd8743ee2cd33b7a57ac72fc8a4872d596e88
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2023-01-08 22:13:51 +0200

    Add Ex10.7: scrollable app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,27 +1,30 @@
-import { View, StyleSheet } from 'react-native'
+import { View, ScrollView, StyleSheet } from 'react-native'
 import Constants from 'expo-constants'
 import theme from '../theme'
 import AppBarTab from './AppBarTab'
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.appBarBackground,
     paddingBottom: 20,
   },
+  scrollContainer: {
+    flexDirection: 'row',
+  },
   child: {
-    marginLeft: 10
-  }
+    marginLeft: 10,
+  },
   // ...
 })
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab style={styles.child} linkTo="/" content={'Repositories'} />
-      <AppBarTab style={styles.child} linkTo="signin" content={'Sign in'} />
+      <ScrollView horizontal style={styles.scrollContainer}>
+        <AppBarTab style={styles.child} linkTo="/" content={'Repositories'} />
+        <AppBarTab style={styles.child} linkTo="signin" content={'Sign in'} />
+      </ScrollView>
     </View>
   )
 }
